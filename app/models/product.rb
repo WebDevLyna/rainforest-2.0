9d096cfa0402d09b2ec1642325e5a692f3a81218
SweetNAMese <t.lyna.nguyen@gmail.com>
2016-05-20 11:21:57 -0400

    Creating review model and associating the user, product and review models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
+  has_many :reviews
+  has_many :users, through: :reviews
+
   # This will prevent users from trying to save undesirable information to the database.
   validates :name, :description, presence: true
   validates :price_in_cents, numericality: {only_integer: true}
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :reviews
+  has_many :products, through: :reviews
   # This will auto add "attr_accessor :password, :password_confirmation" in model
   has_secure_password
 end
